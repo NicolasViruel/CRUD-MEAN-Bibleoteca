a6b2d78e3cc026bcbd7e235b63f4c96b12e9a2d7
nicolasviruel <nicolasviruel@gmail.com>
2024-02-25 19:38:50 -0300

    Se genera el boton de busqueda en la seccion de libros

--- src/app/components/listar-libros/listar-libros.component.ts
+++ src/app/components/listar-libros/listar-libros.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { Libro } from 'src/app/models/libro';
 import { LibrosService } from 'src/app/services/libros.service';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-listar-libros',
@@ -10,25 +12,34 @@ import { LibrosService } from 'src/app/services/libros.service';
 })
 export class ListarLibrosComponent implements OnInit {
   listLibros: Libro[] = [];
+  librosFiltrados: Libro[] = [];
   mostrarSpinner: boolean = false;
+  busqueda= new FormControl();
 
   constructor(private _libroService: LibrosService,
               private toastr: ToastrService) { }
 
   ngOnInit(): void {
     this.obtenerLibros();
+    this.iniciarBusquedaLibro();
   }
 
   // Es importante tener en cuenta que este metodo ordenara los libros directamente en el arreglo listLibros
   //  que se muestra en tu plantilla. Si quiero mantener el arreglo original intacto y mostrar
   //  una lista ordenada en la plantilla, podriamos crear un nuevo arreglo ordenado y asignarlo a listLibros en 
   // lugar de ordenar el arreglo original directamente.
+
   obtenerLibros() {
-    this._libroService.getLibros().subscribe(data => {
-      this.listLibros = data.sort((a: Libro, b: Libro) => a.nombre.localeCompare(b.nombre));
-    }, error => {
-      console.log(error);
-    });
+    this._libroService.getLibros().subscribe(
+      (data: Libro[]) => {
+        this.listLibros = data.sort((a: Libro, b: Libro) => a.nombre.localeCompare(b.nombre));
+        // Inicializa los libros filtrados
+        this.librosFiltrados = [...this.listLibros];
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 
   eliminarLibro(id: any) {
@@ -49,4 +60,24 @@ export class ListarLibrosComponent implements OnInit {
     }
   }
 
+  iniciarBusquedaLibro() {
+    this.busqueda.valueChanges
+      .pipe(debounceTime(800), distinctUntilChanged())
+      .subscribe((data) => {
+        this.mostrarSpinner = false
+        // Filtra la lista completa en base a la busqueda
+        this.librosFiltrados = this.listLibros.filter(
+          (libro) => libro.nombre.toLowerCase().includes(data.toLowerCase())
+        );
+        this.mostrarSpinner = true;
+
+        if (this.librosFiltrados.length === 0) {
+          this.toastr.info('No se encontraron libros para la búsqueda', 'Información');
+        };
+      });
+  }
+
+
 }
+  
+
